1584906adce1202fd030b857cf6a42a09276fa62
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2025-06-25 11:04:51 +1000

    Changed collection.db a bit and added an image and stuff

--- views.py
+++ views.py
@@ -9,9 +9,20 @@ def init_routes(app):
         return render_template('menu.html')
     
 
+    #view all vehicles in database
+    @app.route('/index', methods=['GET'])
+    def index():
+        if request.args.get('name') is not None:
+            name = request.args.get('name')
+            vehicles = Vehicle.query.filter(Vehicle.name.ilike(f'%{name}%')).all()
+        else:
+            vehicles = Vehicle.query.all()
+        return render_template('index.html', vehicles=vehicles)
+    
+
     #view databases
     @app.route('/database', methods=['GET'])
-    def view_databases():
+    def databases():
         if request.args.get('name') is not None:
             name = request.args.get('name')
             databases = Database.query.filter(Database.name.ilike(f'%{name}%')).all()
@@ -26,6 +37,35 @@ def init_routes(app):
         vehicle = Vehicle.query.get(id)
         return render_template('view_vehicle.html', vehicle = vehicle)
     
+
+    #view database
+    @app.route('/view_database', methods=['GET'])
+    def view_database():
+        id = request.args.get('id')
+        database = Database.query.get(id)
+        return render_template('view_database.html', database = database)
+
+#edit database
+    @app.route('/edit', methods=['GET'])
+    def edit_database():
+        #get database
+        id = request.args.get('id')
+        database = Database.query.get(id)
+
+        if request.method == 'GET':
+            return render_template('edit.html', database = database)
+    
+        if request.method == 'POST':
+            
+            id = request.form["id"]
+            database = Vehicle.query.get(id)
+            database.image = request.form.get("Image")
+            database.name = request.form.get("Name")
+            database.description = request.form.get("Description")
+            
+            db.session.commit()
+            return redirect(url_for('database'))
+
 
     #edit vehicle
     @app.route('/edit', methods=['GET'])
@@ -59,6 +99,16 @@ def init_routes(app):
             return redirect(url_for('index'))
         
 
+#delete database
+    @app.route('/delete', methods=['GET'])
+    def delete_database():
+        id = request.args.get('id')
+        database = Database.query.get(id)
+        db.session.delete(database)
+        db.session.commit()
+
+        return redirect(url_for('database'))
+
     #delete vehicle
     @app.route('/delete', methods=['GET'])
     def delete_vehicle():
@@ -76,12 +126,3 @@ def init_routes(app):
     @app.route('/quiz', methods=['GET'])
     def quiz():
         return render_template('quiz.html')
-    
-    @app.route('/index', methods=['GET'])
-    def index():
-        if request.args.get('name') is not None:
-            name = request.args.get('name')
-            vehicles = Vehicle.query.filter(Vehicle.name.ilike(f'%{name}%')).all()
-        else:
-            vehicles = Vehicle.query.all()
-        return render_template('index.html', vehicles=vehicles)
